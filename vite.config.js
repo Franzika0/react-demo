2d653f131954c062d99a72e573c97f9f30b18c4e
Franzika0 <ForHS05.08@gmail.com>
2026-07-14 15:17:20 +0800

    Allow ALB host

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      "kkcompany-aws-hkt-alb-757878951.us-east-1.elb.amazonaws.com"
+    ]
+  }
 })
